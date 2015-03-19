7bd325b8bff1bc3574f9f9d5a7e60e91faf88de8
Tao J <tao-j@outlook.com>
2015-03-19 23:44:45 +0800

    add: translated previous Verilog code to Chisel
    original Verilog code provided by Yamin Li
    slt instruction was added to pass the second test

---
+++ src/main/scala/Memory.scala
@@ -0,0 +1,153 @@
+import Chisel._
+
+class MemoryIO() extends Bundle
+{
+  // TODO: valid wrapper
+  val d_f_mem = UInt(INPUT, 32).flip()   // data from memory
+  val r_m_addr  = UInt(OUTPUT, 32).flip()  // read memory address
+  val w_m_addr  = UInt(OUTPUT, 32).flip()  // write memory address
+  val d_t_mem = UInt(OUTPUT, 32).flip()  // data to memory
+  val w_d_mem = Bool(OUTPUT).flip()  // write data memory
+}
+
+class Memory(resetSignal: Bool = null) extends Module(_reset = resetSignal)
+{
+  val io = new MemoryIO()
+
+  val textSeg = Mem(UInt(width = 32), 64)
+  val dataSeg = Mem(UInt(width = 32), 64)
+
+  /*
+  test program 1 .text at 0x00003000 .data at 0x00000000
+0  : 3c010000; % (00) main:  lui  r1, 0          # address of data[0]     %
+1  : 34240080; % (04)        ori  r4, r1, 0x80   # address of data[0]     %
+2  : 20050004; % (08)        addi r5, r0, 4      # counter                %
+3  : 0c000018; % (0c) call:  jal  sum            # call function          %
+4  : ac820000; % (10)        sw   r2, 0(r4)      # store result           %
+5  : 8c890000; % (14)        lw   r9, 0(r4)      # check sw               %
+6  : 01244022; % (18)        sub  r8, r9, r4     # sub: r8 <-- r9 - r4    %
+7  : 20050003; % (1c)        addi r5, r0, 3      # counter                %
+8  : 20a5ffff; % (20) loop2: addi r5, r5, -1     # counter - 1            %
+9  : 34a8ffff; % (24)        ori  r8, r5, 0xffff # zero-extend: 0000ffff  %
+A  : 39085555; % (28)        xori r8, r8, 0x5555 # zero-extend: 0000aaaa  %
+B  : 2009ffff; % (2c)        addi r9, r0, -1     # sign-extend: ffffffff  %
+C  : 312affff; % (30)        andi r10, r9,0xffff # zero-extend: 0000ffff  %
+D  : 01493025; % (34)        or   r6, r10, r9    # or: ffffffff           %
+E  : 01494026; % (38)        xor  r8, r10, r9    # xor: ffff0000          %
+F  : 01463824; % (3c)        and  r7, r10, r6    # and: 0000ffff          %
+10 : 10a00001; % (40)        beq  r5, r0, shift  # if r5 = 0, goto shift  %
+11 : 08000008; % (44)        j    loop2          # jump loop2             %
+12 : 2005ffff; % (48) shift: addi r5, r0, -1     # r5 = ffffffff          %
+13 : 000543c0; % (4c)        sll  r8, r5, 15     # <<15 = ffff8000        %
+14 : 00084400; % (50)        sll  r8, r8, 16     # <<16 = 80000000        %
+15 : 00084403; % (54)        sra  r8, r8, 16     # >>16 = ffff8000(arith) %
+16 : 000843c2; % (58)        srl  r8, r8, 15     # >>15 = 0001ffff(logic) %
+17 : 08000017; % (5c) finish: j   finish         # dead loop              %
+18 : 00004020; % (60) sum:   add  r8, r0, r0     # sum                    %
+19 : 8c890000; % (64) loop:  lw   r9, 0(r4)      # load data              %
+1A : 20840004; % (68)        addi r4, r4, 4      # address + 4            %
+1B : 01094020; % (6c)        add  r8, r8, r9     # sum                    %
+1C : 20a5ffff; % (70)        addi r5, r5, -1     # counter - 1            %
+1D : 14a0fffb; % (74)        bne  r5, r0, loop   # finish?                %
+1E : 00081000; % (78)        sll  r2, r8, 0      # move result to v0      %
+1F : 03e00008; % (7c)        jr   r31            # return                 %
+   */
+  val prog1 = Array(
+    UInt("h3c010000"),
+    UInt("h34240080"),
+    UInt("h20050004"),
+    UInt("h0c000c18"),
+    UInt("hac820000"),
+    UInt("h8c890000"),
+    UInt("h01244022"),
+    UInt("h20050003"),
+    UInt("h20a5ffff"),
+    UInt("h34a8ffff"),
+    UInt("h39085555"),
+    UInt("h2009ffff"),
+    UInt("h312affff"),
+    UInt("h01493025"),
+    UInt("h01494026"),
+    UInt("h01463824"),
+    UInt("h10a00001"),
+    UInt("h08000c08"),
+    UInt("h2005ffff"),
+    UInt("h000543c0"),
+    UInt("h00084400"),
+    UInt("h00084403"),
+    UInt("h000843c2"),
+    UInt("h08000c17"),
+    UInt("h00004020"),
+    UInt("h8c890000"),
+    UInt("h20840004"),
+    UInt("h01094020"),
+    UInt("h20a5ffff"),
+    UInt("h14a0fffb"),
+    UInt("h00081000"),
+    UInt("h03e00008"),
+    UInt("h_dead_beef")
+  )
+  /*
+  test program 2 .text at 0x00003000 .data at 0x00000000
+ Address    Code        Basic                     Source
+
+0x00003000  0x20010001  addi $1,$0,0x00000001 1          addi $1,$0,1
+0x00003004  0xac010000  sw $1,0x00000000($0)  2          sw   $1,0
+0x00003008  0x00010880  sll $1,$1,0x00000002  3          sll  $1,$1,2
+0x0000300c  0xac010004  sw $1,0x00000004($0)  4          sw   $1,4
+0x00003010  0x00010880  sll $1,$1,0x00000002  5          sll  $1,$1,2
+0x00003014  0xac010008  sw $1,0x00000008($0)  6          sw   $1,8
+0x00003018  0x00000022  sub $0,$0,$0          7          sub  $0,$0,$0     # set reg[0] to 0, use as base
+0x0000301c  0x8c010000  lw $1,0x00000000($0)  8          lw   $1,0($0)     # reg[1] <- mem[0] (= 1)
+0x00003020  0x8c020004  lw $2,0x00000004($0)  9          lw   $2,4($0)     # reg[2] <- mem[4] (= A)
+0x00003024  0x8c030008  lw $3,0x00000008($0)  10         lw   $3,8($0)     # reg[3] <- mem[8] (= B)
+0x00003028  0x00842022  sub $4,$4,$4          11         sub  $4,$4,$4     # reg[4] <- 0, running total
+0x0000302c  0x00442020  add $4,$2,$4          12   loop: add  $4,$2,$4     # reg[4]+ = A
+0x00003030  0x0043282a  slt $5,$2,$3          13         slt  $5,$2,$3     # reg[5] <- A < B
+0x00003034  0x10a00002  beq $5,$0,0x00000002  14         beq  $5,$0,end    # if reg[5] = FALSE, go forward 2 instructions
+0x00003038  0x00221020  add $2,$1,$2          15         add  $2,$1,$2     # A++
+0x0000303c  0x1000fffb  beq $0,$0,0xfffffffb  16         beq  $0,$0,loop   # go back 5 instructions
+0x00003040  0xac040000  sw $4,0x00000000($0)  17   end:  sw   $4,0($0)     # mem[0] <- reg[4]
+0x00003044  0x1000ffff  beq $0,$0,0xffffffff  18   dead: beq  $0,$0,dead   # program is over, keep looping back to here
+   */
+  val prog2 = Array(
+    UInt("h20010001"),
+    UInt("hac010000"),
+    UInt("h00010880"),
+    UInt("hac010004"),
+    UInt("h00010880"),
+    UInt("hac010008"),
+    UInt("h00000022"),
+    UInt("h8c010000"),
+    UInt("h8c020004"),
+    UInt("h8c030008"),
+    UInt("h00842022"),
+    UInt("h00442020"),
+    UInt("h0043282a"),
+    UInt("h10a00002"),
+    UInt("h00221020"),
+    UInt("h1000fffb"),
+    UInt("hac040000"),
+    UInt("h1000ffff"),
+  UInt("h_dead_beef")
+  )
+
+  val prog = prog1
+  for (i <- 0 until prog.length) {
+    textSeg(i) := prog(i)
+  }
+
+  val dout = UInt()
+  val r_word_addr = io.r_m_addr >> UInt(2)
+  val w_word_addr = io.w_m_addr >> UInt(2)
+
+  dout := UInt("h_beefabee")
+  when (io.r_m_addr >= UInt("h00003000", 32)) {
+    dout := textSeg(r_word_addr)
+  }
+    .otherwise {
+    dout := dataSeg(r_word_addr) }
+  when (io.w_d_mem) { dataSeg(w_word_addr) := io.d_t_mem }
+
+  io.d_f_mem := dout
+}
---
+++ src/main/scala/Top.scala
@@ -0,0 +1,319 @@
+import Chisel._
+
+class Top extends Module {
+  val io = new Bundle {
+    val reset = Bool(INPUT)
+  }
+
+  val core = Module(new Core)
+  val memory = Module(new Memory)
+
+  core.io.reset <> io.reset
+  core.io.mem <> memory.io
+}
+
+class TopTests(c: Top) extends Tester(c) {
+  val v1 = rnd.nextInt(1 << 8)
+  val v2 = rnd.nextInt(1 << 8)
+  val v3 = rnd.nextInt(1 << 8)
+  val v4 = rnd.nextInt(1 << 8)
+
+  pokeAt(c.memory.dataSeg, (v1 + v2 + v3 + v4) % (1 << 31), 0)
+  pokeAt(c.memory.dataSeg, v1, 0x80 >> 2)
+  pokeAt(c.memory.dataSeg, v2, 0x84 >> 2)
+  pokeAt(c.memory.dataSeg, v3, 0x88 >> 2)
+  pokeAt(c.memory.dataSeg, v4, 0x8c >> 2)
+
+  poke(c.io.reset, 1)
+  step (1)
+
+  poke(c.io.reset, 0)
+  step (300)
+  //TODO: val prv_pc = peek(c.core.pc)
+
+  peekAt(c.memory.dataSeg, 0x90 >> 2)
+  //printf("%0x\n", (v1 + v2 + v3 + v4) % (1 << 31))
+
+  def see: Unit = {
+    //peek()
+    //peekAt()
+  }
+}
+
+object Top {
+  def main(args: Array[String]): Unit = {
+    val tutArgs = args.slice(1, args.length)
+    args.foreach(arg => println(arg))
+    chiselMainTest(tutArgs, () => Module(new Top())) {
+      c => new TopTests(c) }
+  }
+}
+
+/*
+module multiple_cycle_cpu (cpu_clk, resetn, m_addr, d_f_mem, d_t_mem, w_d_mem, wr_vram, rd_vram, io_rdn, state);
+    input  cpu_clk, resetn; // cpu clock and resetn (active low)
+    output [31:0] m_addr;   // memory address
+    input  [31:0] d_f_mem;  // data to memory
+    output [31:0] d_t_mem;  // data from memory
+    output w_d_mem;         // write data memory
+    output wr_vram;         // write video memory, for vga
+    output rd_vram;         // vram read
+    output io_rdn;          // i/o read (active low), for ps2 keyboard
+    output [2:0] state;     // state for test
+
+    parameter [2:0] s_if  = 3'b000, // IF  state
+                    s_id  = 3'b001, // ID  state
+                    s_exe = 3'b010, // EXE state
+                    s_mem = 3'b011, // MEM state
+                    s_wb  = 3'b100; // WB  state
+
+    // registers
+    reg [31:0] pc = 0;      // program counter
+    reg [31:0] ir = 0;      // instruction register
+    reg [31:0] c  = 0;      // register C
+    reg [31:0] d  = 0;      // register D
+    reg [2:0] state = s_if; // state register
+
+    // instruction format
+    wire [05:00] opcode = ir[31:26];
+    wire [04:00] rs     = ir[25:21];
+    wire [04:00] rt     = ir[20:16];
+    wire [04:00] rd     = ir[15:11];
+    wire [04:00] sa     = ir[10:06];
+    wire [05:00] func   = ir[05:00];
+    wire [15:00] imm    = ir[15:00];
+    wire [25:00] addr   = ir[25:00];
+    wire         sign   = ir[15];
+    wire [31:00] s_imm  = {{16{sign}},imm}; // sign-ext. imm
+    wire [31:00] z_imm  = {16'h0000,imm};   // zero-ext. imm
+
+    // for next_pc
+    wire [31:00] p4_addr = pc + 32'h4;                 // pc+4
+    wire [31:00] br_addr = pc + {s_imm[29:0],2'b00};   // beq, bne
+    wire [31:00] jr_addr = a;                          // jr
+    wire [31:00] jp_addr = {pc[31:28],addr,2'b00};     // j, jal
+
+    // instruction decode
+    wire i_add  = (opcode == 6'h00) & (func == 6'h20); // add
+    wire i_sub  = (opcode == 6'h00) & (func == 6'h22); // sub
+    wire i_and  = (opcode == 6'h00) & (func == 6'h24); // and
+    wire i_or   = (opcode == 6'h00) & (func == 6'h25); // or
+    wire i_xor  = (opcode == 6'h00) & (func == 6'h26); // xor
+    wire i_sll  = (opcode == 6'h00) & (func == 6'h00); // all
+    wire i_srl  = (opcode == 6'h00) & (func == 6'h02); // srl
+    wire i_sra  = (opcode == 6'h00) & (func == 6'h03); // sra
+    wire i_jr   = (opcode == 6'h00) & (func == 6'h08); // jr
+    wire i_addi = (opcode == 6'h08);                   // addi
+    wire i_andi = (opcode == 6'h0c);                   // andi
+    wire i_ori  = (opcode == 6'h0d);                   // ori
+    wire i_xori = (opcode == 6'h0e);                   // xori
+    wire i_lw   = (opcode == 6'h23);                   // lw
+    wire i_sw   = (opcode == 6'h2b);                   // sw
+    wire i_beq  = (opcode == 6'h04);                   // beq
+    wire i_bne  = (opcode == 6'h05);                   // bne
+    wire i_lui  = (opcode == 6'h0f);                   // lui
+    wire i_j    = (opcode == 6'h02);                   // j
+    wire i_jal  = (opcode == 6'h03);                   // jal
+
+    // program counter
+    always @ (posedge cpu_clk or negedge resetn) begin
+        if (!resetn)  pc <= 0;
+        else if (wpc) pc <= next_pc; // wpc
+    end
+
+    // instruction register
+    always @ (posedge cpu_clk or negedge resetn) begin
+        if (!resetn)  ir <= 0;
+        else if (wir) ir <= d_f_mem; // wir
+    end
+
+    // register C
+    always @ (posedge cpu_clk or negedge resetn) begin
+        if (!resetn)  c <= 0;
+        else          c <= c_in;
+    end
+
+    // register D
+    always @ (posedge cpu_clk or negedge resetn) begin
+        if (!resetn)  d <= 0;
+        else          d <= d_f_mem;
+    end
+
+    // state register
+    always @ (posedge cpu_clk or negedge resetn) begin
+        if (!resetn)  state <= s_if;
+        else          state <= next_state;
+    end
+
+    // register file
+    reg  [31:0] regfile [31:1]; // $1 - $31 regs
+    wire [31:0] a = (rs==0) ? 0 : regfile[rs]; // read port 0
+    wire [31:0] b = (rt==0) ? 0 : regfile[rt]; // read port 1
+    always @ (posedge cpu_clk or negedge resetn) begin
+        if (resetn == 0) begin  // reset
+            integer i;
+            for (i = 1; i < 32; i = i + 1)
+                regfile[i] <= 0;
+        end else begin                         // write port
+            if (wreg && (dest_rn != 0)) begin  // wreg
+                regfile[dest_rn] <= data_2_rf;
+            end
+        end
+    end
+
+    // memory-mapped I/O
+    wire io_space = c[31] & // i/o space:
+                   ~c[30] & // a000_0000 -
+                    c[29];  // bfff_ffff
+
+    // vram space, can be done outside of cpu
+    wire vr_space = c[31] & // vram space:
+                    c[30] & // c000_0000 -
+                   ~c[29];  // dfff_ffff
+
+    // output signals
+    assign w_d_mem =   s_mem & i_lw & ~io_space & ~vr_space; // write data memory
+    assign d_t_mem =   b;                                    // write reg[rt] to memory
+    assign io_rdn  = ~(s_mem & i_lw &  io_space);            // i/o read (active low)
+    assign wr_vram =   s_mem & i_sw &  vr_space;             // write  video ram
+    assign rd_vram =   s_mem & i_lw &  vr_space;             // read   video ram
+
+    // will be nets (wires):
+    reg        wpc;           // 0. write PC
+    reg        wir;           // 1. write IR
+    reg        wreg;          // 2. write register file
+    reg        wmem;          // 3. write memory
+    reg [31:0] next_pc;       // 4. next state
+    reg [31:0] c_in;          // 5. to register C
+    reg [31:0] data_2_rf;     // 6. select ALU output
+    reg  [4:0] dest_rn;       // 7. destination register
+    reg  [2:0] next_state;    // 8. next state
+    reg [31:0] m_addr;        // 9. memory address
+
+    // combinational circuit:
+    always @* begin           //    default settings:
+        wpc        = 0;       // 0. do not write PC
+        wir        = 0;       // 1. do not write IR
+        wreg       = 0;       // 2. do not write register file
+        wmem       = 0;       // 3. do not write data memory
+        next_pc    = p4_addr; // 4. next pc = pc + 4
+        c_in       = 0;       // 5. for register C input
+        data_2_rf  = c;       // 6. select ALU output
+        dest_rn    = rd;      // 7. destinarion reg = rd
+        next_state = s_if;    // 8. next state = IF
+        m_addr     = pc;      // 9. memory address = PC
+
+        case (state) //---------------------------------------- IF:
+            s_if: begin             // IF state
+                wpc        = 1;     // write PC
+                wir        = 1;     // write IR
+                next_state = s_id;  // next state: ID
+            end //--------------------------------------------- ID:
+            s_id: begin                   // ID state
+                if (i_j) begin            // j   -> IF
+                    wpc        = 1;       // write PC
+                    next_pc    = jp_addr; // jump address
+                    next_state = s_if;    // next state: IF
+                end else if (i_jal) begin // jal -> IF
+                    wpc        = 1;       // write PC
+                    next_pc    = jp_addr; // jump address
+                    wreg       = 1;       // save return address
+                    dest_rn    = 5'd31;   // to $31
+                    data_2_rf  = pc;      // return address
+                    next_state = s_if;    // next state: IF
+                end else if (i_jr) begin  // jr  -> IF
+                    wpc        = 1;       // write PC
+                    next_pc    = jr_addr; // jump address
+                    next_state = s_if;    // next state: IF
+                end else begin            // other instructions
+                    c_in = br_addr;       // branch address
+                    next_state = s_exe;   // others -> EXE
+                end
+            end //--------------------------------------------- EXE:
+            s_exe: begin                  // EXE state
+            case (1'b1)
+                i_add: begin              // add  -> WB
+                    c_in = a + b;
+                    next_state = s_wb; end
+                i_sub: begin              // sub  -> WB
+                    c_in = a - b;
+                    next_state = s_wb; end
+                i_and: begin              // and  -> WB
+                    c_in = a & b;
+                    next_state = s_wb; end
+                i_or: begin               // or   -> WB
+                    c_in = a | b;
+                    next_state = s_wb; end
+                i_xor: begin              // xor  -> WB
+                    c_in = a ^ b;
+                    next_state = s_wb; end
+                i_sll: begin              // sll  -> WB
+                    c_in = b << sa;
+                    next_state = s_wb; end
+                i_srl: begin              // srl  -> WB
+                    c_in = b >> sa;
+                    next_state = s_wb; end
+                i_sra: begin              // sra  -> WB
+                    c_in = $signed(b) >>> sa;
+                    next_state = s_wb; end
+                i_addi: begin             // addi -> WB
+                    c_in = a + s_imm;
+                    next_state = s_wb; end
+                i_andi: begin             // andi -> WB
+                    c_in = a & z_imm;
+                    next_state = s_wb; end
+                i_ori: begin              // ori  -> WB
+                    c_in = a | z_imm;
+                    next_state = s_wb; end
+                i_xori: begin             // xori -> WB
+                    c_in = a ^ z_imm;
+                    next_state = s_wb; end
+                i_lw: begin               // lw  -> MEM
+                    c_in = a + s_imm;
+                    next_state = s_mem; end
+                i_sw: begin               // sw  -> MEM
+                    c_in = a + s_imm;
+                    next_state = s_mem; end
+                i_beq: begin              // beq  -> IF
+                    if (a == b) begin
+                        next_pc = br_addr;
+                        wpc = 1;
+                    end
+                    next_state = s_if; end
+                i_bne: begin              // bne  -> IF
+                    if (a != b) begin
+                        next_pc = br_addr;
+                        wpc = 1;
+                    end
+                    next_state = s_if; end
+                i_lui: begin              // lui  -> WB
+                    c_in = {imm,16'h0};
+                    next_state = s_wb; end
+                default: next_state = s_if;
+            endcase
+            end //--------------------------------------------- MEM:
+            s_mem: begin                  // MEM state
+                m_addr = c;               // data address
+                if (i_lw) begin           // lw -> WB
+                    next_state = s_wb;    // next state: WB
+                end else begin            // sw -> IF
+                    wmem = 1;             // write memory
+                    next_state = s_if;    // next state: IF
+                end
+            end //--------------------------------------------- WB:
+            s_wb: begin                   // WB state
+                if (i_lw) data_2_rf = d;  // select memory data
+                if (i_lw || i_addi || i_andi || i_ori || i_xori || i_lui)
+                    dest_rn = rt;         // destinarion reg: rt
+                wreg = 1;                 // write register file
+                next_state = s_if;        // next state: IF
+            end //--------------------------------------------- END
+
+            default: begin
+                next_state = s_if;        // default state
+            end
+        endcase
+    end
+endmodule
+
+ */
